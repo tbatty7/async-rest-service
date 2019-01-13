58a242c607503177886562981fcdcb2f88df6b53
Tim B <tbatty7@gmail.com>
2019-01-12 21:23:27 -0500

    programmatically added table and inserted data into it

---
+++ src/main/java/com/example/asyncrestservice/Block.java
@@ -0,0 +1,16 @@
+package com.example.asyncrestservice;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Block {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String texture;
+    private String dropsblock;
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert into block(id, name, texture, dropsblock)
+values('10001', 'dirt block', 'brown', true);
+
+insert into block(id, name, texture, dropsblock)
+values('10002', 'cobblestone', 'grey', true);
+
+insert into block(id, name, texture, dropsblock)
+values('10003', 'grass', 'green', false);
+
